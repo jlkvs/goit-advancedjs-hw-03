9859163d5c7a999dd954b5642403937b28c7ce14
julia <juljuliakv@gmail.com>
2025-06-22 19:57:11 +0200

    Pixabay API image search with Axios

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,21 +1,19 @@
+import axios from 'axios';
+
 const API_KEY = import.meta.env.VITE_PIXABAY_API_KEY;
 const BASE_URL = 'https://pixabay.com/api/';
 
 export async function fetchImages(query) {
-  const params = new URLSearchParams({
+  const params = {
     key: API_KEY,
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
-  });
+  };
 
-  const response = await fetch(`${BASE_URL}?${params}`);
-  if (!response.ok) {
-    throw new Error('Failed to fetch images');
-  }
-
-  return response.json();
+  const response = await axios.get(BASE_URL, { params });
+  return response.data;
 }
 
 
